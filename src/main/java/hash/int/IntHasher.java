8e7bc3364b9801ac8244b6b298ed638a44bc26cb
Tobin Baker <tdbaker@cs.washington.edu>
2018-09-06 15:12:23 -0700

    fix interfaces

--- src/main/java/hash/int/IntHasher.java
+++ src/main/java/hash/int/IntHasher.java
@@ -1,3 +1,4 @@
 public interface IntHasher {
     public int hash(int x);
+    public int unhash(int x);
 }
--- src/main/java/hash/long/LongHasher.java
+++ src/main/java/hash/long/LongHasher.java
@@ -1,3 +1,4 @@
 public interface LongHasher {
     public long hash(long x);
+    public long unhash(long x);
 }
